28a32feeeefdb898fa81d6cd880319481282b0ad
Djoe_ <43145127+PourrezJ@users.noreply.github.com>
2019-10-09 11:53:19 +0200

    Create entrypoint.sh

---
+++ entrypoint.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+cd /home/container
+
+# Make internal Docker IP address available to processes.
+export INTERNAL_IP=`ip route get 1 | awk '{print $NF;exit}'`
+
+# Replace Startup Variables
+MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
+echo ":/home/container$ ${MODIFIED_STARTUP}"
+
+# Fix for Rust not starting
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$(pwd)
